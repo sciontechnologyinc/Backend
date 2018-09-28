7d3c93697cee945befb6e078a2d084e030a4f917
WAJJ FREELANCE <wajjfreelance@gmail.com>
2018-09-27 20:54:58 -0700

    sa

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <br/>
+   <br>
+   <br>
+   <br>
+    <img src="http://gsisadmin.com/storage/school_images/banner.png" />
+</div>
+@endsection
